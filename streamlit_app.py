32f279d4a5f8083d027aa4c0ff828b50be38338f
Francisco José Ruiz Vera <francisco@regionalseguros.com.py>
2024-06-17 15:44:25 -0400

    Fixed to_excel_file function call

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,8 +135,8 @@ def main():
             excel_file = BytesIO()
 
             # Write each dataframe to a different worksheet.
-            excel_data_prima = to_excel(final_df_prima)
-            excel_data_prima_tecnica = to_excel(final_df_prima_tecnica)
+            excel_data_prima = to_excel_file(final_df_prima)
+            excel_data_prima_tecnica = to_excel_file(final_df_prima_tecnica)
 
             # Write the data to the BytesIO object
             excel_file.write(excel_data_prima)
